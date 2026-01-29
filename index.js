bb8fabbe1e420ccb15d59b7105ebbbede6b02cfd
OsmanBurda <osmanceviker0@gmail.com>
2026-01-29 21:35:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,27 +4,19 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Dosyaların nerede olduğunu belirler
+app.use(express.static(__dirname));
 
 let players = {};
-let flags = {
-    red: { x: 50, y: 300, owner: null },
-    blue: { x: 750, y: 300, owner: null }
-};
 
 io.on('connection', (socket) => {
-    // Osman: 3 Can ve Başlangıç Ayarları
+    // Osman: 3 Can ve Takım Ayarları
     players[socket.id] = {
         x: 400, y: 300,
         score: 0, lives: 3,
-        team: null,
-        hasFlag: false
+        team: null
     };
-
-    socket.on('setTeam', (team) => {
-        // Sadece bayrak kapmaca modunda takım seçimi
-        players[socket.id].team = team;
-    });
+    console.log('Bir oyuncu bağlandı:', socket.id);
 
     socket.on('disconnect', () => {
         delete players[socket.id];
@@ -32,4 +24,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Sunucu 3000 portunda hazır!'));
+http.listen(PORT, () => console.log('Sunucu ' + PORT + ' portunda hazır!'));
