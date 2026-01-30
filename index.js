43e74ec8cc7cbdb98c9a1ff1115c81a959faede8
OsmanBurda <osmanceviker0@gmail.com>
2026-01-30 15:39:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,16 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// Dosyaların nerede olduğunu belirler
+// Osman, bu satır oyunun açılmasını sağlar
 app.use(express.static(__dirname));
 
-let players = {};
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 io.on('connection', (socket) => {
-    // Osman: 3 Can ve Takım Ayarları
-    players[socket.id] = {
-        x: 400, y: 300,
-        score: 0, lives: 3,
-        team: null
-    };
-    console.log('Bir oyuncu bağlandı:', socket.id);
-
-    socket.on('disconnect', () => {
-        delete players[socket.id];
-    });
+    console.log('Oyuncu girdi: ' + socket.id);
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Sunucu ' + PORT + ' portunda hazır!'));
+http.listen(PORT, () => console.log('Sunucu hazır!'));
