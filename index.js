3119ecdc53687b808e82614ab6ba582313383fb9
OsmanBurda <osmanceviker0@gmail.com>
2026-01-30 16:02:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-const express = require('express');
+ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// Dosyaların ana klasörde olduğunu belirtir
 app.use(express.static(__dirname));
 
-// Ana sayfaya girildiğinde index.html'i gönderir
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Oyuncu bağlantılarını dinler
 io.on('connection', (socket) => {
-    console.log('Bir oyuncu bağlandı: ' + socket.id);
-    
-    socket.on('disconnect', () => {
-        console.log('Oyuncu çıktı.');
-    });
+    console.log('Oyuncu bağlandı: ' + socket.id);
+    socket.on('disconnect', () => console.log('Oyuncu ayrıldı.'));
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log('Sunucu ' + PORT + ' portunda canavar gibi çalışıyor!');
-});
+http.listen(PORT, () => console.log('Sunucu ' + PORT + ' üzerinde hazır!'));
