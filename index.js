f973a9b71efea5dd2623816bcad716cac312a948
OsmanBurda <osmanceviker0@gmail.com>
2026-01-30 17:24:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,42 +2,37 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const path = require('path');
 
 app.use(express.static(__dirname));
 
 let players = {};
 
 io.on('connection', (socket) => {
-    // Yeni oyuncu bağlandığında varsayılan değerler
+    // Yeni oyuncu bağlandığında
     players[socket.id] = { 
         x: 1500, 
         y: 1500, 
         id: socket.id, 
-        lives: 3, // VS modunda 3 can kuralın
-        name: "Osman", // Senin adın
-        color: "DEFAULT_BLUE" // Burası artık hem renk hem resim linki tutuyor
+        name: "Osman",
+        color: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0NPrkQ7yE9Z08S5XkG9D9Q9J-uG8n7f-GgA&s" // Ronaldo
     };
     
-    // Mevcut oyuncuları yeni gelene gönder
     socket.emit('currentPlayers', players);
 
-    // OYUNCU "BAŞLA" DEDİĞİNDE (Menüden gelen bilgiler)
+    // Başla butonuna basıldığında ismi ve kostümü güncelle
     socket.on('startGame', (data) => {
         if (players[socket.id]) {
             players[socket.id].name = data.name;
-            players[socket.id].color = data.color; // HTML'den gelen resim linkini buraya kaydeder
-            // Herkese bu oyuncunun yeni ismini ve kostümünü (resmini) duyur
+            players[socket.id].color = data.color;
             io.emit('updatePlayerInfo', players[socket.id]);
         }
     });
 
-    // Hareket verilerini al ve herkese yay
+    // Hareket verisi
     socket.on('playerMovement', (movementData) => {
         if (players[socket.id]) {
             players[socket.id].x = movementData.x;
             players[socket.id].y = movementData.y;
-            // Diğer oyunculara senin yerini bildir
             socket.broadcast.emit('playerMoved', players[socket.id]);
         }
     });
@@ -48,6 +43,5 @@ io.on('connection', (socket) => {
     });
 });
 
-// Port ayarı
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Sunucu Kostüm Sistemiyle Başlatıldı!'));
+http.listen(PORT, () => console.log('Ronaldo Kostümü Hazır!'));
