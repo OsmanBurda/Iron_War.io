ad6e90c91b9992198d1d203522bb53fe8d0550c9
OsmanBurda <osmanceviker0@gmail.com>
2026-01-30 16:34:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// Dosyaların yerini garantiye alalım
-const publicPath = path.join(__dirname);
-app.use(express.static(publicPath));
+// Osman, burası beyaz ekranın anahtarı
+app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
-    console.log('Oyuncu bağlandı: ' + socket.id);
+    console.log('Beyaz dünyaya biri katıldı!');
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Sunucu ' + PORT + ' portunda bembeyaz!'));
+http.listen(PORT, () => console.log('Sunucu BEYAZ modda hazır!'));
