858ef6ff9c82394df30f2f08cb70f5c7c93d9c60
OsmanBurda <osmanceviker0@gmail.com>
2026-01-30 16:55:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,27 @@ app.use(express.static(__dirname));
 let players = {};
 
 io.on('connection', (socket) => {
-    players[socket.id] = { x: 1500, y: 1500, id: socket.id, lives: 3 };
+    // Oyuncu ilk bağlandığında geçici veriler
+    players[socket.id] = { 
+        x: 1500, 
+        y: 1500, 
+        id: socket.id, 
+        lives: 3,
+        name: "Adsız",      // Varsayılan İsim
+        color: "#3498db"    // Varsayılan Renk (Mavi)
+    };
+    
     io.emit('currentPlayers', players);
+
+    // OYUNCU "BAŞLA" DEDİĞİNDE BİLGİLERİ GÜNCELLE
+    socket.on('startGame', (data) => {
+        if (players[socket.id]) {
+            players[socket.id].name = data.name;
+            players[socket.id].color = data.color;
+            // Herkese bu oyuncunun yeni ismini ve rengini duyur
+            io.emit('updatePlayerInfo', players[socket.id]);
+        }
+    });
 
     socket.on('playerMovement', (movementData) => {
         if (players[socket.id]) {
@@ -27,4 +46,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Sunucu Hazır!'));
+http.listen(PORT, () => console.log('Sunucu Menü Moduyla Hazır!'));
