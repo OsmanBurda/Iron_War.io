d08c6d6aadb3c9f4937125aaa67406cc73f67290
OsmanBurda <osmanceviker0@gmail.com>
2026-01-30 16:11:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
- const express = require('express');
+const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
 app.use(express.static(__dirname));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 io.on('connection', (socket) => {
-    console.log('Oyuncu bağlandı: ' + socket.id);
-    socket.on('disconnect', () => console.log('Oyuncu ayrıldı.'));
+    console.log('Bağlantı başarılı: ' + socket.id);
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Sunucu ' + PORT + ' üzerinde hazır!'));
+http.listen(PORT, () => console.log('Sunucu 3000 portunda çalışıyor.'));
