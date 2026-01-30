702574ed9ed45d8dfa9ce76ef4b787a632c06fa5
OsmanBurda <osmanceviker0@gmail.com>
2026-01-30 17:17:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,29 +9,35 @@ app.use(express.static(__dirname));
 let players = {};
 
 io.on('connection', (socket) => {
+    // Yeni oyuncu bağlandığında varsayılan değerler
     players[socket.id] = { 
         x: 1500, 
         y: 1500, 
         id: socket.id, 
-        lives: 3,
-        name: "Adsız",
-        color: "#3498db"
+        lives: 3, // VS modunda 3 can kuralın
+        name: "Osman", // Senin adın
+        color: "DEFAULT_BLUE" // Burası artık hem renk hem resim linki tutuyor
     };
     
-    io.emit('currentPlayers', players);
+    // Mevcut oyuncuları yeni gelene gönder
+    socket.emit('currentPlayers', players);
 
+    // OYUNCU "BAŞLA" DEDİĞİNDE (Menüden gelen bilgiler)
     socket.on('startGame', (data) => {
         if (players[socket.id]) {
             players[socket.id].name = data.name;
-            players[socket.id].color = data.color;
+            players[socket.id].color = data.color; // HTML'den gelen resim linkini buraya kaydeder
+            // Herkese bu oyuncunun yeni ismini ve kostümünü (resmini) duyur
             io.emit('updatePlayerInfo', players[socket.id]);
         }
     });
 
+    // Hareket verilerini al ve herkese yay
     socket.on('playerMovement', (movementData) => {
         if (players[socket.id]) {
             players[socket.id].x = movementData.x;
             players[socket.id].y = movementData.y;
+            // Diğer oyunculara senin yerini bildir
             socket.broadcast.emit('playerMoved', players[socket.id]);
         }
     });
@@ -42,5 +48,6 @@ io.on('connection', (socket) => {
     });
 });
 
+// Port ayarı
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Sunucu Hazır!'));
+http.listen(PORT, () => console.log('Sunucu Kostüm Sistemiyle Başlatıldı!'));
