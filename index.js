f69eaad4f1b2647aeef75363f9facd875e258476
OsmanBurda <osmanceviker0@gmail.com>
2026-01-31 10:52:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ io.on('connection', (socket) => {
     players[socket.id] = { 
         x: 1500, y: 1500, id: socket.id, 
         name: "Osman", 
-        color: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe_nMarGj4bTgbT3erB8NoJGVamlFJFQf5KNAuWOvfnQ&s=10" 
+        color: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe_nMarGj4bTgbT3erB8NoJGVamlFJFQf5KNAuWOvfnQ&s=10",
+        sawAngle: 0,
+        sawCount: 1
     };
+    
     socket.emit('currentPlayers', players);
+
     socket.on('startGame', (data) => {
         if (players[socket.id]) {
             players[socket.id].name = data.name;
@@ -21,20 +25,25 @@ io.on('connection', (socket) => {
             io.emit('updatePlayerInfo', players[socket.id]);
         }
     });
+
     socket.on('playerMovement', (mv) => {
         if (players[socket.id]) {
             players[socket.id].x = mv.x;
             players[socket.id].y = mv.y;
+            players[socket.id].sawAngle = mv.sawAngle;
+            players[socket.id].sawCount = mv.sawCount;
             socket.broadcast.emit('playerMoved', players[socket.id]);
         }
     });
+
     socket.on('disconnect', () => {
         delete players[socket.id];
         io.emit('playerDisconnected', socket.id);
     });
 });
 
+// Render için kritik düzeltme:
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Sunucu ${PORT} portunda aktif.`);
 });
