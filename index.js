fae5350245cc205f38befce110e90707d6c63273
OsmanBurda <osmanceviker0@gmail.com>
2026-01-31 18:10:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname));
 
 function createWorld() {
     let objects = [];
-    for(let i = 0; i < 2500; i++) {
+    for(let i = 0; i < 1500; i++) {
         let rx = Math.random() * MAP_SIZE;
         let ry = Math.random() * MAP_SIZE;
         let dist = Math.sqrt(Math.pow(rx - 5000, 2) + Math.pow(ry - 5000, 2));
@@ -24,20 +24,32 @@ let worldObjects = createWorld();
 
 io.on('connection', (socket) => {
     socket.on('join', (data) => {
-        players[socket.id] = { id: socket.id, x: 5000, y: 5000, name: data.name || "Osman", angle: 0 };
+        players[socket.id] = { id: socket.id, x: 5000, y: 5000, name: data.name || "Osman", angle: 0, score: 0 };
         socket.emit('init', { objects: worldObjects, id: socket.id, mapSize: MAP_SIZE });
     });
+
     socket.on('move', (data) => {
         if (players[socket.id]) {
             players[socket.id].x = data.x;
             players[socket.id].y = data.y;
             players[socket.id].angle = data.angle;
+            
+            // Eşya toplama kontrolü (Basit mesafe kontrolü)
+            worldObjects.forEach((obj, index) => {
+                let d = Math.sqrt(Math.pow(obj.x - data.x, 2) + Math.pow(obj.y - data.y, 2));
+                if (d < 40) {
+                    players[socket.id].score += 10;
+                    worldObjects.splice(index, 1);
+                    io.emit('objectCollected', { id: obj.id, playerId: socket.id });
+                }
+            });
         }
     });
+
     socket.on('disconnect', () => { delete players[socket.id]; });
 });
 
 setInterval(() => { io.emit('update', players); }, 20);
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log("Server dinleniyor..."));
+const PORT = process.env.PORT || 3000; [cite: 2026-01-26]
+http.listen(PORT, () => console.log("Iron War Hazir!"));
