823178d673609cc7c773e5c02dd9aab308b6a066
OsmanBurda <osmanceviker0@gmail.com>
2026-01-31 17:33:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-const MAP_SIZE = 10000; // Harita boyutu tam 10000
-const CENTER = 5000;
+const MAP_SIZE = 10000; // Harita tam 10000 [cite: 2026-01-27]
 
 app.use(express.static(__dirname));
 
@@ -13,10 +12,11 @@ function createWorld() {
     for(let i = 0; i < 2500; i++) {
         let rx = Math.random() * MAP_SIZE;
         let ry = Math.random() * MAP_SIZE;
-        let dist = Math.sqrt(Math.pow(rx - CENTER, 2) + Math.pow(ry - CENTER, 2));
+        let dist = Math.sqrt(Math.pow(rx - 5000, 2) + Math.pow(ry - 5000, 2));
         
+        // Bölgeye göre eşyalar: 1500px elmas, 3800px altın, dışarısı demir (metal)
         let tier = dist < 1500 ? "E" : (dist < 3800 ? "A" : "N");
-        let kind = Math.random() > 0.9 ? "JEN" : (Math.random() > 0.8 ? "KASA" : (Math.random() > 0.7 ? "DISLI" : "PARA"));
+        let kind = Math.random() > 0.8 ? "JEN" : (Math.random() > 0.6 ? "KASA" : "PARA");
 
         objects.push({ id: i, x: rx, y: ry, kind: kind, tier: tier });
     }
@@ -28,14 +28,15 @@ let players = {};
 
 io.on('connection', (socket) => {
     socket.on('join', (data) => {
-        players[socket.id] = { id: socket.id, x: CENTER, y: CENTER, name: data.name, angle: 0 };
-        socket.emit('init', { objects: worldObjects, id: socket.id });
+        players[socket.id] = { id: socket.id, x: 5000, y: 5000, name: data.name || "Osman", angle: 0 };
+        socket.emit('init', { objects: worldObjects, id: socket.id, mapSize: MAP_SIZE });
     });
 
     socket.on('move', (data) => {
         if (players[socket.id]) {
-            players[socket.id].x = data.x;
-            players[socket.id].y = data.y;
+            // Duvar Kontrolü: 0-10000 dışına çıkamaz
+            players[socket.id].x = Math.max(0, Math.min(MAP_SIZE, data.x));
+            players[socket.id].y = Math.max(0, Math.min(MAP_SIZE, data.y));
             players[socket.id].angle = data.angle;
         }
     });
