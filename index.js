23c01047d7030b77f73374a50c970e1033868e1b
OsmanBurda <osmanceviker0@gmail.com>
2026-01-31 11:08:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ app.use(express.static(__dirname));
 let players = {};
 
 io.on('connection', (socket) => {
+    // 10000x10000 haritanın tam ortasında (5000, 5000) doğuş
     players[socket.id] = { 
-        x: 2500, y: 2500, id: socket.id, 
+        x: 5000, y: 5000, id: socket.id, 
         name: "Osman", 
         color: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe_nMarGj4bTgbT3erB8NoJGVamlFJFQf5KNAuWOvfnQ&s=10",
         sawAngle: 0,
@@ -44,5 +45,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, '0.0.0.0', () => {
-    console.log(`Sunucu ${PORT} üzerinde Radar açık!`);
+    console.log(`Sunucu 10000x10000 harita ve Radar ile hazır!`);
 });
