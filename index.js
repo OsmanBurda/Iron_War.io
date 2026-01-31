eec1aa164fedcb8731357a5a77d1086fbf1aec26
OsmanBurda <osmanceviker0@gmail.com>
2026-01-31 14:34:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,17 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// OSMAN'IN BÖLGE AYARLARI
 const MAP_SIZE = 10000;
 const CENTER = 5000;
 const ELMAS_RADIUS = 1500; 
 const ALTIN_RADIUS = 3800;
 
 app.use(express.static(__dirname));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
 
 function createWorld() {
     let objects = [];
-    for(let i = 0; i < 2000; i++) {
+    for(let i = 0; i < 2500; i++) {
         let rx = Math.random() * MAP_SIZE;
         let ry = Math.random() * MAP_SIZE;
         let dist = Math.sqrt(Math.pow(rx - CENTER, 2) + Math.pow(ry - CENTER, 2));
@@ -29,17 +25,13 @@ function createWorld() {
 
         const r = Math.random();
         let kind = r > 0.95 ? "JEN" : (r > 0.85 ? "KASA" : (r > 0.70 ? "DISLI" : "PARA"));
-
         objects.push({ id: i, x: rx, y: ry, kind: kind, tier: tier });
     }
     return objects;
 }
 
 let worldObjects = createWorld();
-
-io.on('connection', (socket) => {
-    socket.emit('init', { objects: worldObjects, mapSize: MAP_SIZE });
-});
+io.on('connection', (socket) => { socket.emit('init', { objects: worldObjects }); });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => { console.log('Sunucu Hazır!'); });
+http.listen(PORT, () => { console.log('Sunucu Osman için hazır!'); });
